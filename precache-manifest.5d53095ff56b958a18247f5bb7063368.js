0fbb9ce27d1f89f2801effc3e463f1d5599ca4a5
Wiermag <wierzbowska.magda@gmail.com>
2020-05-04 17:17:31 +0200

    Updates

---
+++ precache-manifest.5d53095ff56b958a18247f5bb7063368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655d79f29c7ec52c881f5601217537fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e46e03adb8125da659b3",
+    "url": "./static/css/main.e0d8d12a.chunk.css"
+  },
+  {
+    "revision": "99f5fe5b202817709133",
+    "url": "./static/js/2.5a80541b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.5a80541b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46e03adb8125da659b3",
+    "url": "./static/js/main.de70780c.chunk.js"
+  },
+  {
+    "revision": "17fb633d79e0ec858374",
+    "url": "./static/js/runtime-main.5aa82065.js"
+  },
+  {
+    "revision": "eacc8ef1fe4157cac38a54671d37b7bf",
+    "url": "./static/media/close-up-of-hand-holding-pencil-over-white-background-316466.eacc8ef1.jpg"
+  }
+]);
